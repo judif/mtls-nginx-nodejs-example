e12bed4d9be31a1597124679a3e2808f542c2d61
Judith <judith.freiberger@gmail.com>
2021-03-14 12:30:26 +0100

    Verify certificates in request header and restructure

--- server/server.js
+++ server/server.js
@@ -1,24 +1,29 @@
 const express = require('express');
-const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
 const port = 3000;
 
 const options = {
-    // ca: fs.readFileSync(path.resolve(__dirname, '../certs/ca_crt.pem')),
-    // cert: fs.readFileSync(path.resolve(__dirname, '../certs/server_crt.pem')),
-    // key: fs.readFileSync(path.resolve(__dirname, '../certs/server_key.pem')),
-    // rejectUnauthorized: true,
-    // requestCert: true,
+    ca: fs.readFileSync(path.resolve(__dirname, './certs/ca.crt')),
+    cert: fs.readFileSync(path.resolve(__dirname, './certs/server.crt')),
+    key: fs.readFileSync(path.resolve(__dirname, './certs/server.key')),
+    rejectUnauthorized: false,
+    requestCert: true,
 };
 
 const app = express();
 
 app.get('/', (req, res) => {
-   res.status(200).json({ 'msg': `Hello, it is ${new Date().toISOString()}`});
+    console.log(req.headers)
+
+    if(req.header("ssl_client_verify") !== "SUCCESS")
+        return res.status(403).send("Forbidden - please provide valid certificate.")
+
+    res.status(200).json(`Hello ${req.header("ssl_client")}, your certificate was issued by ${req.header("SSL_Client_Issuer")}!`);
 });
 
-const server = http.createServer(options, app).listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+https.createServer(options, app).listen(port, () => {
+    console.log(`.. server up and running and listening on ${port} ..`);
 });
